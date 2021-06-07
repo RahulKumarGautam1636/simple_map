17f6f8c6879584e3bca6eec667ff7bcd056fe0cb
Rahul Kumar Gautam <71900374+RahulKumarGautam1636@users.noreply.github.com>
2021-06-07 22:45:01 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -2,12 +2,12 @@ var myIp = $("#myIp");
 var myLocation = $("#myLocation");
 var myTimezone = $("#myTimezone");
 var myIsp = $("#myIsp");
+
 function showDetails(item) {
    myIp.text(item.ip);
    myLocation.text(item.city);
    myTimezone.text(item.timezone);
    myIsp.text(item.org);
-   myFunction(x);
 }
 function closeInfo() {
   $(".user_info").toggleClass("hideInfo");
@@ -24,8 +24,7 @@ function getLocation() {
 
 function findLocation(position) {
   newLocation = [position.coords.latitude, position.coords.longitude];
-  newmarker = L.marker(newLocation, {icon: myIcon});
-  renderThis(newmarker, "Your GPS location.", myGroup);
+  myLocationList("MyGPSLocation", newLocation);
 }
 
 function showLocation(response) {
@@ -33,7 +32,8 @@ function showLocation(response) {
   var lat = response.loc.split(",")[0];
   var long = response.loc.split(",")[1];
   showDetails(response);
-  getMap([lat, long]);
+  ipLocation = [lat, long];
+  getMap(ipLocation);
   },"jsonp")
 }
 
@@ -57,16 +57,13 @@ function getMap(i) {
       iconUrl: 'images/icon-location.svg',
       iconSize: [38, 55],
   });
-  marker = L.marker(i, {icon: myIcon});
-  myGroup = L.layerGroup();
-  renderThis(marker, "You are here.", myGroup);
   placesGroup = L.layerGroup();
+  myLocationList("MyIPLocation", i);
   newPlaceList();
 }
 
 function newPlaceList() {
   mymap.on('click', function(e) {
-    console.log(e.latlng);
 var item = prompt("Enter name for this place.");
 var   place = {
       name: item,
@@ -77,41 +74,61 @@ var   place = {
   });
 }
 
+function myLocationList(itemName, coords) {
+  var   place = {
+        name: itemName,
+        coord: coords
+      }
+      places.push(place);
+      renderPlaces(places);
+}
+
+function createList(name) {
+   div = document.createElement('div');
+   li = document.createElement('li');
+   a = document.createElement('a');
+   del = document.createElement('img');
+  del.setAttribute("src", "images/icon-cross.svg");
+  div.appendChild(a);
+  div.appendChild(del);
+  li.appendChild(div);
+  ul.appendChild(li);
+  a.innerText = name;
+  a.href = '#';
+}
+
+const ul = document.querySelector("#placeList");
 function renderPlaces(i) {
-  const ul = document.querySelector("#placeList");
   while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
   }
   i.forEach((item) => {
-    const div = document.createElement('div');
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const del = document.createElement('img');
-    del.setAttribute("src", "images/icon-cross.svg");
+    createList(item.name);
     del.classList.add(item.name);
     del.onclick = function () {
     placesGroup.clearLayers();
     deletePlace(i, this.className);
     return;
     };
-    a.innerText = item.name;
-    a.href = '#';
-    var markers = L.marker(item.coord, {icon: myIcon});
+var  markers = L.marker(item.coord, {icon: myIcon});
     renderThis(markers, item.name, placesGroup);
+    markers.openPopup();
     a.addEventListener('click', () => {
-      flyToPlace(item.coord);
+      flyToPlace(item.coord, item.name);
       toggleMenu();
     });
-    div.appendChild(a);
-    div.appendChild(del);
-    li.appendChild(div);
-    ul.appendChild(li);
   });
   placesGroup.addTo(mymap);
 }
 
-function flyToPlace(c) {
-  mymap.flyTo(c, 14, {duration: 3})
+function flyToPlace(c, p) {
+  mymap.flyTo(c, 14, {duration: 3});
+  setTimeout(() => {
+    L.popup({ closeButton: false, offset: L.point(0, -17) })
+   .setLatLng(c)
+   .setContent(p)
+   .openOn(mymap);
+}, 3000);
 }
 
 function popUpContent(content, layer) {
@@ -124,57 +141,40 @@ function toggleMenu() {
 }
 
 var recenter = $(".reCenter_container");
-var checkGpsOn = false;
-function reCenter() {
-if (checkGpsOn) {
-    mymap.flyTo(newLocation, 10, {duration: 3});
+var isGPSon = false;
+recenter.on('click', function() {
+  if (isGPSon) {
+    flyToPlace(newLocation, "MyGPSLocation");
   } else {
-    myGroup.clearLayers();
-    getLocation();
-    checkGpsOn = true;
+    flyToPlace(ipLocation, "MyIPLocation");
   }
-}
+});
+recenter.on('dblclick', function(e) {
+  if (!isGPSon) {
+    getLocation();
+    isGPSon = true;
+  }
+});
 
 function renderThis(mark, popUp, group) {
-  mark.bindPopup(popUp, { closeButton: false, offset: L.point(0, -15) }).openPopup();
+  mark.bindPopup(popUp, { closeButton: false, offset: L.point(0, -17) }).openPopup();
   group.addLayer(mark);
   group.addTo(mymap);
   mark.on('dblclick', function(e) {
-    mymap.flyTo(e.latlng, 14, {duration: 3})
+    flyToPlace(e.latlng, popUp);
   });
 }
 
-// var findThis = document.querySelector(".inputBar");
+var findThis = document.querySelector(".inputBar");
 function findNew() {
-//   console.log(findThis.value);
-//   jQuery.get("https://tools.keycdn.com/geo.json?host={2409:4060:413:aed1:9441:95f3:c779:c45f}", function(response) {
-//   // var lat = response.loc.split(",")[0];
-//   // var long = response.loc.split(",")[1];
-//   console.log(response);
-//   // getMap([lat, long]);
-//   },"jsonp")
-   
-   $.ajax({
-         url: "https://rahulkumargautam1636.github.io/simple_map/",
-         type: "GET",
-         crossDomain: true,
-         success: function (response) {
-               console.log(response);
-         },
-         error: function (xhr, status) {
-             alert("error");
-         }
-     });
-
-}
-
-x = window.matchMedia("(max-width: 900px)");
-myFunction(x);
-x.addListener(myFunction);
-function myFunction(x) {
-    if (!x.matches) {
-      $(".user_info").addClass("desk_height");
-    } else {
-      $(".user_info").addClass("mobile_height");
-    }
+  console.log(findThis.value);
+  jQuery.get("https://geo.ipify.org/api/v1?apiKey=at_nf6K91Bp5z0IdHWzspRMGJdnrm0hy&ipAddress="+findThis.value, function(response) {
+  console.log(response);
+var  searchedLocation = [response.location.lat, response.location.lng];
+  searchedMarker = L.marker(searchedLocation, {icon: myIcon});
+  searchedGroup = L.layerGroup();
+  renderThis(searchedMarker, response.as.domain, searchedGroup);
+  flyToPlace(searchedLocation);
+  searchedMarker.openPopup();
+  })
 }
